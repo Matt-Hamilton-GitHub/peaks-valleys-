0ad31a0f08426e482e16b3937a0d0353f45ebeb7
Matt Hamilton <66541468+Matt-Hamilton-GitHub@users.noreply.github.com>
2021-10-25 01:48:05 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//Write and test a function that counts the number of peaks and the number of valleys in a dynamic,
+//sequential structure of integers. A peak is an element of the structure that is greater than its two adjacent neighbors. 
+//A valley is an element of the structure that is less than its two adjacent neighbors. 
+//The first and last elements of the structure are adjacent neighbors.
+//The function returns the number of peaks and the number of valleys in the structure. 
+//The function has only one parameter – the sequential structure, returns only one structure – the number of peaks 
+//and the number of valleys, uses neither global nor file scope user-defined objects, and uses neither input nor output streams.
+
+#include <iostream>
+
+int main()
+{
+	system("pause");
+	return 0;
+}
